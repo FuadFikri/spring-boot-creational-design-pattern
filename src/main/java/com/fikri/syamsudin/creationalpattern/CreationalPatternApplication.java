e7eddff6f8460976ae0abb40ffecc8604ec95f56
fikri <fuadfikri01@gmail.com>
2021-12-30 20:57:01 +0700

    initial commit

---
+++ src/main/java/com/fikri/syamsudin/creationalpattern/CreationalPatternApplication.java
@@ -0,0 +1,13 @@
+package com.fikri.syamsudin.creationalpattern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreationalPatternApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreationalPatternApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fikri/syamsudin/creationalpattern/CreationalPatternApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fikri.syamsudin.creationalpattern;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreationalPatternApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
